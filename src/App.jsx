a3cd3ae678fd9731ffbff6c87a00e904ddf01620
Pablo L <pabloleite03@gmail.com>
2024-05-29 23:00:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { SupplierProvider } from "./context/SupplierContext";
+import Home from "./pages/Home";
+import AddSupplier from "./pages/AddSupplier";
+import EditSupplier from "./pages/EditSupplier";
+import SupplierDetails from "./components/SupplierDetails";
+import "antd/dist/reset.css";
+import "./index.css";
+
+const App = () => {
   return (
-    <div>
-      <h1>Vite + React</h1>
-    </div>
+    <Router>
+      <SupplierProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddSupplier />} />
+          <Route path="/edit/:id" element={<EditSupplier />} />
+          <Route path="/details/:id" element={<SupplierDetails />} />
+        </Routes>
+      </SupplierProvider>
+    </Router>
   );
-}
+};
 
 export default App;
