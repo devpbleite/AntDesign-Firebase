3a7b559584cd75032491f7c05cb5b88cee3ef1fc
Pablo Leite <pabloleite03@gmail.com>
2024-05-29 22:31:39 -0300

    feat: add api file.

---
+++ src/services/api.js
@@ -0,0 +1,46 @@
+let suppliers = JSON.parse(localStorage.getItem("suppliers")) || [
+  { id: 1, name: "Fornecedor A", email: "fornecedorA@example.com" },
+  { id: 2, name: "Fornecedor B", email: "fornecedorB@example.com" },
+];
+
+const saveToLocalStorage = (data) => {
+  localStorage.setItem("suppliers", JSON.stringify(data));
+};
+
+export const getSuppliers = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(suppliers), 500);
+  });
+};
+
+export const addSupplier = (supplier) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      suppliers = [...suppliers, { id: Date.now(), ...supplier }];
+      saveToLocalStorage(suppliers);
+      resolve();
+    }, 500);
+  });
+};
+
+export const updateSupplier = (updatedSupplier) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      suppliers = suppliers.map((supplier) =>
+        supplier.id === updatedSupplier.id ? updatedSupplier : supplier
+      );
+      saveToLocalStorage(suppliers);
+      resolve();
+    }, 500);
+  });
+};
+
+export const deleteSupplier = (id) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      suppliers = suppliers.filter((supplier) => supplier.id !== id);
+      saveToLocalStorage(suppliers);
+      resolve();
+    }, 500);
+  });
+};
